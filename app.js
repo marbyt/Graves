e12f7bdaa047ff2bc28ee6449c2b56ff2cb39903
marek.banny <marek.banny@gmail.com>
2020-01-04 19:45:45 +0100

    adding languages to the page

--- app.js
+++ app.js
@@ -1,1 +1,71 @@
-$('#menu-items').load('menu.html');
+
+
+let currentLanguage = "PL";
+const texts = [
+    {
+        lang: "PL",
+        translations: {
+            firstName: "Imię",
+            lastName: "Nazwisko",
+            findGrave: "Znajdź grób",
+            home: "Indeks"
+        }
+    },
+    {
+        lang: "EN",
+        translations: {
+            firstName: "Name",
+            lastName: "Last Name",
+            findGrave: "Find grave",
+            home: "Home"
+
+        }
+    }
+];
+
+
+
+$(document).ready(changeLanguage())
+//const texts = definedTexts;
+
+function changeLanguage() {
+    currentLanguage = currentLanguage === "PL" ? "EN" : "PL";
+    const languageButton = document.querySelector("#language");
+    if (currentLanguage === "PL") {
+        languageButton.innerText = "ENGLISH";
+    } else {
+        languageButton.innerText = "POLSKI";
+    }
+
+    changeTranslations();
+}
+
+function changeTranslations() {
+    const elements = document.querySelectorAll("[app-lang]");
+    let textId;
+    elements.forEach(item => {
+        textId = item.getAttribute("app-lang");
+        //  console.log(texts.find(lang => lang.lang === currentLanguage).translations);
+        const text = texts.find(
+            lang => lang.lang === currentLanguage
+        ).translations[textId];
+
+        if (item.nodeName === 'INPUT') {
+            item.setAttribute("placeholder", text);
+        } else {
+            item.textContent = text;
+        }
+
+    });
+}
+
+
+
+$('#menu-items').load('menu.html').then(() => {
+    changeLanguage();
+});
+$('#search-graves').load('search-graves.html').then(() => {
+    changeLanguage();
+});
+
+
---
+++ partials.js
@@ -0,0 +1,2 @@
+$('#menu-items').load('menu.html');
+$('#search-graves').load('search-graves.html');
